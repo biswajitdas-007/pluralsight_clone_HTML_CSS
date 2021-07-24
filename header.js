97339dd0a6925e2b48e344c2d1a47960e2db6090
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-07-24 21:07:14 +0530

    fixed the nav bar

--- header.js
+++ header.js
@@ -13,26 +13,27 @@ function slideshow() {
   }, 2000);
 }
 slideshow();
+var count = 0;
 
-// function up(arr) {
-//   let div = document.getElementById(arr).innerText;
-//   if (div == "expand_more") {
-//     document.getElementById(arr).innerText = "expand_less";
-//   }
-//   if (div == "expand_less") {
-//     document.getElementById(arr).innerText = "expand_more";
-//   }
-// }
+function up_product() {
+  let div = document.getElementById("product").innerText;
+  if (div == "expand_more") {
+    document.getElementById("product").innerText = "expand_less";
+  }
+  document.getElementById("nav_down_1").style.display = "block";
+  document.getElementById("body").style.position = "fixed";
+}
+function cut1(id) {
+  document.getElementById("nav_down_1").style.display = "none";
+  document.getElementById("body").style.position = "relative";
+  document.getElementById(id).innerText = "expand_more";
+}
 
-function up(arr, p1) {
+function up(arr) {
   let div = document.getElementById(arr).innerText;
   if (div == "expand_more") {
     document.getElementById(arr).innerText = "expand_less";
   }
-  // if (div == "expand_less") {
-  //   document.getElementById(arr).innerText = "expand_more";
-  // }
-
   document.getElementById("nav_down").style.display = "block";
   document.getElementById("body").style.position = "fixed";
 }
@@ -40,4 +41,26 @@ function cut(id) {
   document.getElementById("nav_down").style.display = "none";
   document.getElementById("body").style.position = "relative";
   document.getElementById(id).innerText = "expand_more";
+}
+
+function nav_down_sign() {
+  let div = document.getElementById("sigin_in").innerText;
+  if (div == "expand_more") {
+    document.getElementById("sigin_in").innerText = "expand_less";
+  }
+  document.getElementById("nav_down_1_sign").style.display = "block";
+  document.getElementById("body").style.position = "fixed";
+}
+
+function cut2() {
+  document.getElementById("nav_down_1_sign").style.display = "none";
+  document.getElementById("body").style.position = "relative";
+  document.getElementById("sigin_in").innerText = "expand_more";
+}
+
+function courses_page() {
+  window.location.href = "courses.html";
+}
+function sign_page() {
+  window.location.href = "signin.html";
 }
